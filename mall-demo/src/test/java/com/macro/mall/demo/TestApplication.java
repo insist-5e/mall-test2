54069015705bcc0b07c2881b315feef1ccc08fb0
insist-5e <liusiyu132720@icloud.com>
2021-11-16 21:09:47 +0800

    version 3

--- mall-demo/src/test/java/com/macro/mall/demo/TestApplication.java
+++ mall-demo/src/test/java/com/macro/mall/demo/TestApplication.java
@@ -1,18 +1,24 @@
 package com.macro.mall.demo;
 
+import com.macro.mall.common.service.RedisService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestApplication {
 
-
+    @Resource
+    RedisService redisService;
 
     @Test
     public void TestFirst(){
-
+        System.out.println(redisService!=null);
     }
 }
